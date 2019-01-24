a507f848cb8be3299cbd982b6d772b36e9a31f5e
Franck Rochat <fun13@live.fr>
2019-01-24 19:08:57 +0100

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo Bonjour, Chargement du Programme "ProjetBrassard" en cours...
 sleep 30
+echo Lancement du Programme "ProjetBrassard".
 sudo python /home/pi/ProjetBrassard/BrassardMain.py &
